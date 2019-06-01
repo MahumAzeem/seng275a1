b53c95cfa5975cdfe31a1ba2698b19c7839d2cd0
Rafay Chaudhry <rafay_chaudhry@live.com>
2019-05-31 19:14:09 -0700

    re-added test for right and bottom border

--- src/test/java/PlayingFieldTest.java
+++ src/test/java/PlayingFieldTest.java
@@ -97,6 +97,30 @@ class PlayingFieldTest {
 				assertEquals(theField.getCurrentPiece().getX(),1);
     }
 
+		@Test
+    void A1PieceTestRight(){
+				PieceFactory factory = new IPieceFactory();
+				PlayingField theField = new PlayingField(factory, new NullResultCollector());
+				// move the piece to the right boundary
+				theField.getCurrentPiece().setX(9);
+				// Try moving into the right boundary
+				theField.moveRight();
+				// Should be 9 because the piece cannot move
+				assertEquals(theField.getCurrentPiece().getX(),9);
+    }
+
+		@Test
+		void A1PieceTestDown(){
+				PieceFactory factory = new IPieceFactory();
+				PlayingField theField = new PlayingField(factory, new NullResultCollector());
+				// move the piece to the bottom boundary
+				theField.getCurrentPiece().setY(20);
+				// Try moving into the bottom boundary
+				theField.moveDown();
+				// Should be 9 because the piece cannot move
+				assertEquals(theField.getCurrentPiece().getY(),20);
+		}
+
 
     @Test
    void A1ScoringTest(){
